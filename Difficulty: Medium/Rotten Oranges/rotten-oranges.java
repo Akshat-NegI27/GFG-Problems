1b58dc510aed14ef729003a43db51a98667e092e
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-04-03 11:47:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            int mat[][] = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) mat[i][j] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            int ans = obj.orangesRotting(mat);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int freshCount = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+                if (grid[i][j] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        if (freshCount == 0) return 0;
+
+        int minutes = 0;
+        int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean rottedThisMinute = false;
+
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+                for (int[] dir : directions) {
+                    int x = curr[0] + dir[0];
+                    int y = curr[1] + dir[1];
+
+                    if (x >= 0 && y >= 0 && x < rows && y < cols && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        freshCount--;
+                        queue.offer(new int[]{x, y});
+                        rottedThisMinute = true;
+                    }
+                }
+            }
+
+            if (rottedThisMinute) minutes++;
+        }
+
+        return freshCount == 0 ? minutes : -1;
+    }
+}
