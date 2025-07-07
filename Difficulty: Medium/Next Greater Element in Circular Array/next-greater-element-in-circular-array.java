165c5e7b431f17ece7dafd84eb0fc7f06d0f38ad
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-07-07 21:56:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>(Collections.nCopies(n, -1));
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int idx = i % n;
+
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[idx]) {
+                stack.pop();
+            }
+
+            if (i < n) {
+                if (!stack.isEmpty()) {
+                    result.set(idx, arr[stack.peek()]);
+                }
+            }
+
+            stack.push(idx);
+        }
+
+        return result;
+    }
+}
