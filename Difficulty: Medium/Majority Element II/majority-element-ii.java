25f0adba7e6fd7451ae993174473b5c03d03b3af
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-07-26 11:13:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element II/majority-element-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (arr == null || arr.length == 0) return result;
+
+        int n = arr.length;
+
+        int candidate1 = -1, candidate2 = -1;
+        int count1 = 0, count2 = 0;
+
+        for (int num : arr) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+        for (int num : arr) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        int threshold = n / 3;
+        if (count1 > threshold) result.add(candidate1);
+        if (count2 > threshold) result.add(candidate2);
+
+        Collections.sort(result); 
+        return result;
+    }
+}
