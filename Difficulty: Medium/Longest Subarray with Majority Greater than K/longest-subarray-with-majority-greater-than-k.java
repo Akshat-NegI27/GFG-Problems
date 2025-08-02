8d15e9065e956da93e12da3488a2d95c18ddfb07
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-08-02 12:22:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
@@ -1,22 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
 class Solution {
-    static int longestSubarray(int[] arr, int k) {
+    public int longestSubarray(int[] arr, int k) {
         int n = arr.length;
+
         int[] diff = new int[n];
         for (int i = 0; i < n; i++) {
-            diff[i] = (arr[i] > k) ? 1 : -1;
+            diff[i] = arr[i] > k ? 1 : -1;
         }
 
         Map<Integer, Integer> map = new HashMap<>();
-        int maxLen = 0, prefixSum = 0;
+        map.put(0, -1); 
+
+        int maxLen = 0;
+        int prefixSum = 0;
 
         for (int i = 0; i < n; i++) {
             prefixSum += diff[i];
@@ -24,46 +19,13 @@ class Solution {
             if (prefixSum > 0) {
                 maxLen = i + 1;
             } else {
-                if (!map.containsKey(prefixSum)) {
-                    map.put(prefixSum, i);
-                }
-
                 if (map.containsKey(prefixSum - 1)) {
                     maxLen = Math.max(maxLen, i - map.get(prefixSum - 1));
                 }
             }
+            map.putIfAbsent(prefixSum, i);
         }
 
         return maxLen;
     }
 }
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            int n = tokens.length;
-            int[] arr = new int[n];
-
-            int i = 0;
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                arr[i] = Integer.parseInt(token);
-                i++;
-            }
-
-            int k = Integer.parseInt(br.readLine().trim());
-            System.out.println(new Solution().longestSubarray(arr, k));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
