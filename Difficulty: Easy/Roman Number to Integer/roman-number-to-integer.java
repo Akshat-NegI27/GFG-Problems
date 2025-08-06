e4774dac66dd0460e06be09a268bbee3dd5220c0
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-08-06 23:51:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int romanToDecimal(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        java.util.Map<Character, Integer> romanMap = new java.util.HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            int currentValue = romanMap.get(s.charAt(i));
+
+            if (i < n - 1) {
+                int nextValue = romanMap.get(s.charAt(i + 1));
+                if (currentValue < nextValue) {
+                    result -= currentValue;
+                } else {
+                    result += currentValue;
+                }
+            } else {
+                result += currentValue;
+            }
+        }
+        return result;
+    }
+}
