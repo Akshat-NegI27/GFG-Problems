24e54e12a677d04d8664c7028f3bfa1de0f2e1fe
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-08-12 18:50:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shop in Candy Store/shop-in-candy-store.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ArrayList<Integer> minMaxCandy(int[] prices, int k) {
+        Arrays.sort(prices);
+        int n = prices.length;
+
+        int minCost = 0, maxCost = 0;
+        int i = 0, j = n - 1;
+        while (i <= j) {
+            minCost += prices[i]; 
+            i++;
+            j -= k; 
+        }
+
+        i = 0; 
+        j = n - 1;
+        while (i <= j) {
+            maxCost += prices[j]; 
+            j--;
+            i += k; 
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(minCost);
+        result.add(maxCost);
+        return result;
+    }
+}
