469a4271fb602c80a7586090cdac2a39df1bf663
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-08-31 15:40:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.java
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.java
@@ -1,45 +1,21 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().maxWater(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
-
-
 class Solution {
-
-    public int maxWater(int[] arr) {
-        int area = 0; 
-        int left = 0, right = arr.length - 1; 
+    public int maxWater(int arr[]) {
+        int left = 0, right = arr.length - 1;
+        int maxArea = 0;
 
         while (left < right) {
-            int h = Math.min(arr[left], arr[right]); 
-            int w = right - left;                   
-            area = Math.max(area, h * w);           
+            int height = Math.min(arr[left], arr[right]);
+            int width = right - left;
+            int area = height * width;
+            maxArea = Math.max(maxArea, area);
 
-            if (arr[left] > arr[right]) {
-                right--;  
+            if (arr[left] < arr[right]) {
+                left++;
             } else {
-                left++;   
+                right--;
             }
         }
 
-        return area; 
+        return maxArea;
     }
 }
