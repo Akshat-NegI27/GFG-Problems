bd3d0eccc12058b00e756e742e7b5ea053c6f5b6
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-09-14 13:10:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.java
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -1,70 +1,15 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            ArrayList<Integer> array2 = new ArrayList<Integer>();
-
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            line = read.readLine();
-            tokens = line.split(" ");
-            for (String token : tokens) {
-                array2.add(Integer.parseInt(token));
-            }
-
-            // ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] gas = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) gas[idx++] = i;
-
-            int[] cost = new int[array2.size()];
-            idx = 0;
-            for (int i : array2) cost[idx++] = i;
-
-            int ans = new Solution().startStation(gas, cost);
-
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int startStation(int[] gas, int[] cost) {
-        // Your code here
         int n = gas.length;
-        
-        int sum = 0;
-        int start = 0;
-        for(int i=0; i<2*n; i++){
-            int ind = i%n;
-            sum+=(gas[ind]-cost[ind]);
-            if(sum<0){
-                sum = 0;
-                start = i+1;
-            }
-            if(i-start==n){
-                return start;
+        int total = 0, tank = 0, start = 0;
+        for (int i = 0; i < n; i++) {
+            total += gas[i] - cost[i];
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                tank = 0;
+                start = i + 1;
             }
         }
-        return -1;
+        return total >= 0 ? start % n : -1;
     }
 }
