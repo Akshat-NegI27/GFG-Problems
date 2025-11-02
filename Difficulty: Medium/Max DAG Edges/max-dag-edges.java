ed426722fdc3f327134fa555481301fccc2e095b
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-11-02 21:05:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max DAG Edges/max-dag-edges.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxEdgesToAdd(int V, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+
+        int[] indegree = new int[V];
+        for (int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            indegree[e[1]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < V; i++)
+            if (indegree[i] == 0)
+                q.add(i);
+
+        List<Integer> topo = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topo.add(node);
+            for (int next : adj.get(node)) {
+                indegree[next]--;
+                if (indegree[next] == 0)
+                    q.add(next);
+            }
+        }
+
+        boolean[][] connected = new boolean[V][V];
+        for (int[] e : edges)
+            connected[e[0]][e[1]] = true;
+
+        int count = 0;
+        for (int i = 0; i < V; i++) {
+            for (int j = i + 1; j < V; j++) {
+                int u = topo.get(i);
+                int v = topo.get(j);
+                if (!connected[u][v])
+                    count++;
+            }
+        }
+        return count;
+    }
+}
