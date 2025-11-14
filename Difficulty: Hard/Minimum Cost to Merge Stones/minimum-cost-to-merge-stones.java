164b1eba723e5c1b17679a9b206dded2f647199d
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2025-11-14 19:01:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum Cost to Merge Stones/minimum-cost-to-merge-stones.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static int mergeStones(int[] stones, int k) {
+        int n = stones.length;
+
+        if ((n - 1) % (k - 1) != 0) return -1;
+
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) prefix[i + 1] = prefix[i] + stones[i];
+
+        int[][] dp = new int[n][n];
+        final int INF = Integer.MAX_VALUE / 2;
+
+        for (int len = k; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                dp[i][j] = INF;
+
+                for (int m = i; m < j; m += (k - 1)) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][m] + dp[m + 1][j]);
+                }
+
+                if ((j - i) % (k - 1) == 0) {
+                    dp[i][j] += prefix[j + 1] - prefix[i];
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
