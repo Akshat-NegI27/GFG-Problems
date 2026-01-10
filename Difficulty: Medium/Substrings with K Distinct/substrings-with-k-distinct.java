f5b8f250f55f5c5f38fa0f50ab27c5bf5e95efa3
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-01-10 14:18:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
@@ -1,34 +1,21 @@
-// User function Template for Java
-
 class Solution {
     public int countSubstr(String s, int k) {
-        return countAtMostK(s, k) - countAtMostK(s, k - 1);
+        if (k == 0) return 0;
+        return atMost(s, k) - atMost(s, k - 1);
     }
-
-    private int countAtMostK(String s, int k) {
-        int n = s.length();
-        int left = 0, right = 0;
-        int count = 0;
-        Map<Character, Integer> freqMap = new HashMap<>();
-
-        for (right = 0; right < n; right++) {
-            char c = s.charAt(right);
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
-
-            // Shrink the window until we have at most k distinct characters
-            while (freqMap.size() > k) {
-                char leftChar = s.charAt(left);
-                freqMap.put(leftChar, freqMap.get(leftChar) - 1);
-                if (freqMap.get(leftChar) == 0) {
-                    freqMap.remove(leftChar);
-                }
-                left++;
+    
+    private int atMost(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0, distinct = 0, res = 0;
+        for (int right = 0; right < s.length(); right++) {
+            int r = s.charAt(right) - 'a';
+            if (freq[r]++ == 0) distinct++;
+            while (distinct > k) {
+                int l = s.charAt(left++) - 'a';
+                if (--freq[l] == 0) distinct--;
             }
-
-            // Add all substrings ending at right with at most k distinct characters
-            count += right - left + 1;
+            res += right - left + 1;
         }
-
-        return count;
+        return res;
     }
 }
