6bdebcdf1335e23f1261784301d070a3266d00d2
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-01-14 15:42:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.java
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.java
@@ -1,31 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int catchThieves(char[] arr, int k) {
-        int n = arr.length, count = 0;
-        Queue<Integer> police = new LinkedList<>();
-        Queue<Integer> thief = new LinkedList<>();
+        ArrayList<Integer> police = new ArrayList<>();
+        ArrayList<Integer> thieves = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 'P') {
-                police.add(i);
-            } else if (arr[i] == 'T') {
-                thief.add(i);
-            }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 'P') police.add(i);
+            else thieves.add(i);
+        }
 
-            while (!police.isEmpty() && !thief.isEmpty()) {
-                if (Math.abs(police.peek() - thief.peek()) > k) {
-                    if (police.peek() < thief.peek()) {
-                        police.poll();
-                    } else {
-                        thief.poll();
-                    }
-                } else {
-                    // match found
-                    police.poll();
-                    thief.poll();
-                    count++;
-                }
+        int i = 0, j = 0, count = 0;
+
+        while (i < police.size() && j < thieves.size()) {
+            if (Math.abs(police.get(i) - thieves.get(j)) <= k) {
+                count++;
+                i++;
+                j++;
+            } else if (police.get(i) < thieves.get(j)) {
+                i++;
+            } else {
+                j++;
             }
         }
 
