a9b0dbd9fa0f40690d122e6a1f99b3c7f9402f18
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-02-28 11:25:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the closest pair from two arrays/find-the-closest-pair-from-two-arrays.java
@@ -0,0 +1,34 @@
+class Solution {
+    public static ArrayList<Integer> findClosestPair(int arr1[], int arr2[], int x) {
+        int n = arr1.length;
+        int m = arr2.length;
+
+        int i = 0, j = m - 1;
+        long minDiff = Long.MAX_VALUE;
+
+        int bestA = 0, bestB = 0;
+
+        while (i < n && j >= 0) {
+            long sum = (long) arr1[i] + arr2[j];
+            long diff = Math.abs(sum - x);
+
+            if (diff < minDiff) {
+                minDiff = diff;
+                bestA = arr1[i];
+                bestB = arr2[j];
+            }
+
+            if (sum > x) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(bestA);
+        result.add(bestB);
+
+        return result;
+    }
+}
