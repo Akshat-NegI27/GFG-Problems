699093655a7e29fe37bf2f58e252aff5a97390a4
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-03-06 14:22:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
@@ -1,43 +1,26 @@
-import java.util.*;
-
 class Solution {
-    public static String smallestWindow(String s, String p) {
+    public static String minWindow(String s, String p) {
         if (s.length() < p.length()) return "";
-
-        int[] need = new int[256]; 
-        int[] have = new int[256]; 
-
-        for (char c : p.toCharArray()) {
-            need[c]++;
-        }
-
-        int required = p.length(); 
-        int left = 0, minLen = Integer.MAX_VALUE, start = -1;
-
+        
+        int[] freq = new int[26];
+        for (char c : p.toCharArray()) freq[c - 'a']++;
+        
+        int left = 0, count = p.length(), minLen = Integer.MAX_VALUE, start = 0;
+        
         for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-
-            have[c]++;
-            if (need[c] > 0 && have[c] <= need[c]) {
-                required--;
-            }
-
-            while (required == 0) {
-
+            if (freq[s.charAt(right) - 'a']-- > 0) count--;
+            
+            while (count == 0) {
                 if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
                     start = left;
                 }
-
-                char leftChar = s.charAt(left);
-                have[leftChar]--;
-                if (need[leftChar] > 0 && have[leftChar] < need[leftChar]) {
-                    required++;
-                }
+                
+                if (++freq[s.charAt(left) - 'a'] > 0) count++;
                 left++;
             }
         }
-
-        return start == -1 ? "" : s.substring(start, start + minLen);
+        
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
