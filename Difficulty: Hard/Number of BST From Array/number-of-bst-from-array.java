b5c91ba42b5e3bb90d48e13026dec5e064efd5a1
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-03-21 18:45:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.java
+++ Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.java
@@ -1,23 +1,30 @@
 class Solution {
     public ArrayList<Integer> countBSTs(int[] arr) {
         int n = arr.length;
-        int[] sorted = arr.clone();
-        Arrays.sort(sorted);
-        HashMap<Integer,Integer> rank = new HashMap<>();
-        for (int i = 0; i < n; i++) rank.put(sorted[i], i);
+        ArrayList<Integer> res = new ArrayList<>();
+        
         int[] catalan = new int[n + 1];
         catalan[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            long v = 0;
-            for (int j = 0; j < i; j++) v += (long)catalan[j] * catalan[i - j - 1];
-            catalan[i] = (int)v;
+        catalan[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                catalan[i] += catalan[j] * catalan[i - j - 1];
+            }
         }
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int x : arr) {
-            int left = rank.get(x);
-            int right = n - left - 1;
-            result.add(catalan[left] * catalan[right]);
+
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (arr[j] < arr[i]) left++;
+                if (arr[j] > arr[i]) right++;
+            }
+
+            res.add(catalan[left] * catalan[right]);
         }
-        return result;
+
+        return res;
     }
 }
