d216260193cafb33173d84a94e96b997b9502e21
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-04-04 23:28:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gray Code/gray-code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ArrayList<String> graycode(int n) {
+        ArrayList<String> res = new ArrayList<>();
+        int size = 1 << n;
+
+        for (int i = 0; i < size; i++) {
+            int gray = i ^ (i >> 1);
+            String bin = Integer.toBinaryString(gray);
+
+            while (bin.length() < n) {
+                bin = "0" + bin;
+            }
+
+            res.add(bin);
+        }
+
+        return res;
+    }
+}
