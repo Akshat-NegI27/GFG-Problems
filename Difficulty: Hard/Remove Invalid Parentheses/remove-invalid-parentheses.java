6b4ef7328d13573ce422c04dab89a5ab08e96a3f
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-05-08 15:11:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Remove Invalid Parentheses/remove-invalid-parentheses.java
@@ -0,0 +1,50 @@
+
+class Solution {
+    public List<String> validParenthesis(String s) {
+        List<String> res = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+
+        q.add(s);
+        visited.add(s);
+        boolean found = false;
+
+        while (!q.isEmpty()) {
+            String cur = q.poll();
+
+            if (isValid(cur)) {
+                res.add(cur);
+                found = true;
+            }
+
+            if (found) continue;
+
+            for (int i = 0; i < cur.length(); i++) {
+                char c = cur.charAt(i);
+                if (c != '(' && c != ')') continue;
+
+                String next = cur.substring(0, i) + cur.substring(i + 1);
+
+                if (!visited.contains(next)) {
+                    visited.add(next);
+                    q.add(next);
+                }
+            }
+        }
+
+        Collections.sort(res);
+        return res;
+    }
+
+    boolean isValid(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') count++;
+            else if (c == ')') {
+                if (count == 0) return false;
+                count--;
+            }
+        }
+        return count == 0;
+    }
+}
