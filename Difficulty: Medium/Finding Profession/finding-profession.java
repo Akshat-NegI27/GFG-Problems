7fa7aa2aa42095f43b0adf83fd5eaedebe112ff9
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-06-07 16:29:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Finding Profession/finding-profession.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String profession(int level, int pos) {
+        int flips = Integer.bitCount(pos - 1);
+
+        return (flips & 1) == 0 ? "Engineer" : "Doctor";
+    }
+}
