9267a562af95feddf0392102d0622a78342a3752
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-06-14 22:41:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exit Point in a Matrix/exit-point-in-a-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> exitPoint(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int i = 0, j = 0;
+        int dir = 0; 
+
+        while (true) {
+
+            if (mat[i][j] == 1) {
+                dir = (dir + 1) % 4;
+                mat[i][j] = 0;
+            }
+
+            int ni = i, nj = j;
+
+            if (dir == 0) nj++;
+            else if (dir == 1) ni++;
+            else if (dir == 2) nj--;
+            else ni--;
+
+            if (ni < 0 || ni >= n || nj < 0 || nj >= m) {
+                return Arrays.asList(i, j);
+            }
+
+            i = ni;
+            j = nj;
+        }
+    }
+}
