6a0ab184a2c723d5be6ceb9ac89fb61f8349a83e
Akshat Negi <141574274+Akshat-NegI27@users.noreply.github.com>
2026-08-01 22:27:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max After m Range Increments/max-after-m-range-increments.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMax(int n, int[] a, int[] b, int[] k) {
+        long[] diff = new long[n + 1];
+
+        for (int i = 0; i < a.length; i++) {
+            diff[a[i]] += k[i];
+            if (b[i] + 1 < n) {
+                diff[b[i] + 1] -= k[i];
+            }
+        }
+
+        long cur = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            cur += diff[i];
+            ans = Math.max(ans, cur);
+        }
+
+        return (int) ans;
+    }
+}
